79001dacc6d909f6ae0c2be0c6c53b2b42bc64af
salmanfxrsi <salmanxprivate@gmail.com>
2025-11-27 23:22:08 +0600

    feat: add basic routing for GET /todos and POST /todos/create-todo in Node.js server

--- Express & Mongoose/Node.JS Code Modules/todo-app/server.js
+++ Express & Mongoose/Node.JS Code Modules/todo-app/server.js
@@ -1,8 +1,15 @@
 const http = require("http");
 
 const server = http.createServer((req, res) => {
-  console.log({req, res});
-  res.end("Welcome to the Todo App Server!");
+  console.log(req.url, req.method);
+  // res.end("Welcome to the Todo App Server!");
+  if (req.url === "/todos" && req.method === "GET") {
+    res.end("All Todos Fetched Successfully!");
+  } else if (req.url === "/todos/create-todo" && req.method === "POST") {
+    res.end("Todo Created Successfully!");
+  } else {
+    res.end("Route Not Found!");
+  }
 });
 
 server.listen(5000, "127.0.0.1", () => {
