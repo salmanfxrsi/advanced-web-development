4ba48075ae37fa6ae137e9f7488c87ef5b95c702
Salman Farsi <salmanfarsi9005@gmail.com>
2026-01-06 00:05:44 +0600

    feat: add parse body utils and add post route in products

--- Next Level Web Development/Be A Express & Mongoose Master/Revisit Important Concepts Of Node and Express/src/controller/product.controller.ts
+++ Next Level Web Development/Be A Express & Mongoose Master/Revisit Important Concepts Of Node and Express/src/controller/product.controller.ts
@@ -1,5 +1,7 @@
 import { IncomingMessage, ServerResponse } from "http";
 import { products } from "../data/product";
+import { parseBody } from "../utils/parseBody";
+import { IProduct } from "../types/product";
 
 export const handleProducts = async (
   req: IncomingMessage,
@@ -9,14 +11,16 @@ export const handleProducts = async (
   if (req.method === "GET" && url === "/products") {
     res.writeHead(200, { "content-type": "application/json" });
     res.end(JSON.stringify(products));
-  }
-  else if(req.method === "POST" && url === "/products")
-  {
-    try{
-        // const body = parseBody(req);
-    }catch (err) {
-        res.writeHead(400);
-        res.end('error occurred')
+  } else if (req.method === "POST" && url === "/products") {
+    try {
+      const body = await parseBody(req);
+      const newProduct: IProduct = { id: 21, name: body.name };
+      products.push(newProduct);
+      res.writeHead(201, { "content-type": "application/json" });
+      res.end(JSON.stringify(products));
+    } catch (err) {
+      res.writeHead(400);
+      res.end("error occurred");
     }
   }
 };
